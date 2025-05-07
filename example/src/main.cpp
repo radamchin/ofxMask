ab0b77e601240524d652a80f590c9c6d4ee24376
Adam Hinshaw <adam@adamhinshaw.com>
2025-05-07 14:05:39 +1000

    Tweak example to draw alpha and luminance variants + draw maser/maskee and the mask.

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,9 +1,16 @@
 #include "ofMain.h"
 #include "ofApp.h"
+#include "ofAppGLFWWindow.h"
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+    
+    ofGLFWWindowSettings settings;
+    settings.setGLVersion(3, 2); // Use programmable renderer
+    settings.setSize(1024,768);
+    
+    ofCreateWindow(settings);
+	// ofSetupOpenGL(1024,1024,OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -1,19 +1,64 @@
 #include "ofApp.h"
 #include "ofxMask.h"
 
-ofxMask mask_;
+ofxMask alpha_mask;
+ofxMask lumi_mask;
+
 //--------------------------------------------------------------
 void ofApp::setup(){
-	mask_.allocate(300,300, ofxMask::LUMINANCE);
-	mask_.beginMask();
-	ofDrawCircle(100,100,50);
-	ofDrawCircle(200,100,50);
-	ofDrawCircle(200,200,50);
-	ofDrawCircle(100,200,50);
-	mask_.endMask();
-	mask_.begin();
-	ofDrawRectangle(100,100,100,100);
-	mask_.end();
+    
+    // Setup a Alpha Mask
+	alpha_mask.allocate(300,300, ofxMask::ALPHA, ofFbo::maxSamples()); // alt. ofxMask::LUMINANCE
+    
+    // Create the mask
+	alpha_mask.beginMask();
+        ofSetColor(255,255);
+        ofDrawCircle(100,100,50);
+    
+        ofSetColor(255,192);
+        ofDrawCircle(200,100,50);
+    
+        ofSetColor(255,128);
+        ofDrawCircle(200,200,50);
+    
+        ofSetColor(255,96);
+        ofDrawCircle(100,200,50);
+	alpha_mask.endMask();
+    
+    // Draw something with the mask
+	alpha_mask.begin();
+        ofSetColor(255,255);
+        ofTranslate(150,150);
+        ofRotateDeg(45);
+        ofSetRectMode(OF_RECTMODE_CENTER);
+        ofDrawRectangle(0,0,120,120);
+	alpha_mask.end();
+    
+    
+    // Setup a Luminance Mask
+    lumi_mask.allocate(300,300, ofxMask::LUMINANCE, ofFbo::maxSamples());
+    
+    lumi_mask.beginMask();
+        ofSetColor(255,255);
+        ofDrawCircle(100,100,50);
+    
+        ofSetColor(255,192);
+        ofDrawCircle(200,100,50);
+    
+        ofSetColor(255,128);
+        ofDrawCircle(200,200,50);
+    
+        ofSetColor(255,96);
+        ofDrawCircle(100,200,50);
+    lumi_mask.endMask();
+    
+    lumi_mask.begin();
+        ofSetColor(255,255);
+        ofTranslate(150,150);
+        ofRotateDeg(45);
+        ofSetRectMode(OF_RECTMODE_CENTER);
+        ofDrawRectangle(0,0,120,120);
+    lumi_mask.end();
 }
 
 //--------------------------------------------------------------
@@ -22,13 +67,40 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    ofBackgroundGradient( ofColor(32,32,32), ofColor::darkGreen);
+    
 	ofPushMatrix();
-	mask_.drawMasker();
+    ofDrawBitmapString("Alpha Masker", 20, 40);
+    alpha_mask.drawMasker();
+    
 	ofTranslate(300,0);
-	mask_.drawMaskee();
+    ofDrawBitmapString("Alpha Maskee", 20, 40);
+	alpha_mask.drawMaskee();
+    
 	ofTranslate(300,0);
-	mask_.draw();
+    ofDrawBitmapString("Alpha Mask", 20, 40);
+	alpha_mask.draw();
+    
 	ofPopMatrix();
+    
+    // Draw luminence mask
+    ofPushMatrix();
+    
+    ofTranslate(0,300);
+    ofDrawBitmapString("Luminance Masker", 20, 40);
+    lumi_mask.drawMasker();
+    
+    ofTranslate(300,0);
+    ofDrawBitmapString("Luminance Maskee", 20, 40);
+    lumi_mask.drawMaskee();
+    
+    ofTranslate(300,0);
+    ofDrawBitmapString("Luminance Mask", 20, 40);
+    lumi_mask.draw();
+    
+    ofPopMatrix();
+    
+    
 }
 
 //--------------------------------------------------------------
